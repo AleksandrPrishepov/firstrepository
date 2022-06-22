7dcbd61935a3904cb16321ccbee3472293dbdbd8
AleksandrPrishepov <sanyaprishepov89@gmail.com>
2022-06-22 23:33:45 +0300

    exam

--- Exam_3_Прищепов.py
+++ Exam_3_Прищепов.py
@@ -21,45 +21,37 @@ from math import pi
 from abc import ABC, abstractmethod
 
 
-class Mathematics(ABC):
+class Figure(ABC):
 
     @abstractmethod
     def figure_area(self):
         pass
 
-class Figure(Mathematics):
-
-    def __init__(self,radius=None,lenth_1=None,lenth_2=None):
-        self.radius = radius
-        self.lenth_1 = lenth_1
-        self.lenth_2 = lenth_2
-
-
 class Circle(Figure):
 
-    def __init__(self,radius,lenth_1,lenth_2):
-        super().__init__(radius, lenth_1,lenth_2)
+    def __init__(self,radius):
+        self.radius = radius
 
     def figure_area(self):
-        return round(self.radius * pi, 2)
+        return round(self.radius**2 * pi, 2)
 
 class Square(Figure):
 
-    def __init__(self,radius,lenth_1,lenth_2):
-        super().__init__(lenth_1, radius, lenth_2)
+    def __init__(self, length):
+        self.length = length
 
     def figure_area(self):
-        return self.lenth_1**2
+        return self.length**2
 
 class Rectangle(Figure):
 
-    def __init__(self,radius,lenth_1,lenth_2):
-        super().__init__(radius, lenth_1, lenth_2)
+    def __init__(self,length1,length2 ):
+        self.length1 = length1
+        self.length2 = length2
 
     def figure_area(self):
-        return self.lenth_1 * self.lenth_2
+        return self.length1 * self.length2
 
-# не получилось вводить атрибутов разное количество в разных объектах
 square = Square(4)
 rectangle = Rectangle(2,6)
 circle = Circle(3)
